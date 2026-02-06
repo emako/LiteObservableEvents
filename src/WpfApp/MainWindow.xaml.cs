891e390a1904e709fc8a8659443d68b6bfe40643
ema <mccoy39082@163.com>
2026-02-06 17:50:40 +0800

    Revert "Rename UnsubscribeAll to UnsubscribeAllOf"
    This reverts commit 27cad554fd1627d2cadf85b94a1be1270ba843d1.

--- src/WpfApp/MainWindow.xaml.cs
+++ src/WpfApp/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ public partial class MainWindow : Window
         this.Events().Closed
             .Subscribe(_ =>
             {
-                WeakReferenceEventHub.Default.UnsubscribeAllOf(this);
+                WeakReferenceEventHub.Default.UnsubscribeAll(this);
                 ViewModel?.Dispose();
             });
 
